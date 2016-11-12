50da0751d391c695513389f29a7457f17f70c3f3
pexpawlo <pexpawlo@gmail.com>
2016-11-12 14:53:38 +0100

    test

--- app/src/main/java/praca/inz/terapiaautyzm/MainActivity.java
+++ app/src/main/java/praca/inz/terapiaautyzm/MainActivity.java
@@ -2,9 +2,13 @@ package praca.inz.terapiaautyzm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+//testujemy
+
 
 public class MainActivity extends AppCompatActivity {
 
+
+    public void a;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
